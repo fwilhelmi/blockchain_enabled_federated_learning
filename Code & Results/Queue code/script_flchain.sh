8509e38c191f83ef35cb7ce665f58ace6ff2b503
Francisco Wilhelmi <franciscowilhelmi@MacBook-Pro.localdomain>
2021-12-21 09:44:01 +0100

    Added the script used to generate simulation results.

--- Code & Results/Queue code/script_flchain.sh
+++ Code & Results/Queue code/script_flchain.sh
@@ -1,11 +1,14 @@
 # define execution parameters
-SIM_TIME=1000000
+SIM_TIME=100000
 LOGS_ENABLED=0
-QUEUE_SIZE=10
-BATCH_SIZE_ARRAY=( 1 2 3 4 5 6 7 8 9 10 )
-LAMBDA_ARRAY=( 2.5 5 7.5 10 12.5 15 )
-MU=15
-TIMEOUT_MINING=1
+QUEUE_SIZE=1000
+
+LAMBDA_ARRAY=( 0.02 0.2 2 20 )
+BATCH_SIZE_ARRAY=($(seq 1 1 100)) 
+MU_ARRAY=(0.01 0.05 0.1 0.2 0.3 0.5 1)
+TIMEOUT_MINING=1000
+N_MINERS=10
+C_P2P=50000000
 SEED=12345
 
 # compile code
@@ -20,16 +23,19 @@ rm output/*
 # execute files
 cd main
 pwd
+for mu in "${MU_ARRAY[@]}"
+do
 for lambda in "${LAMBDA_ARRAY[@]}"
 do 
 for batch_size in "${BATCH_SIZE_ARRAY[@]}"
 do 
 	echo ""
 	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	echo "- EXECUTING lambda = ${lambda}, block size = ${batch_size}"
-	./queue_main $SIM_TIME $LOGS_ENABLED $QUEUE_SIZE ${batch_size} ${lambda} $MU $TIMEOUT_MINING $SEED ../output/script_output_l${lambda}_s${batch_size}.txt
+	echo "- EXECUTING lambda = ${lambda/,/.}, block size = ${batch_size}"
+	./queue_main $SIM_TIME $LOGS_ENABLED $QUEUE_SIZE ${batch_size} ${lambda/,/.} ${mu/,/.} $TIMEOUT_MINING $N_MINERS $C_P2P $SEED ../output/script_output_m${mu/,/.}_l${lambda/,/.}_s${batch_size}.txt
 	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 	echo ""
+done
 done
 done
 echo ""
